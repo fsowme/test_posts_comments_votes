7d7b472c6aefa731e8c6dd4da785a3d2b4626c69
Vitalii <fsow@yandex.ru>
2021-10-20 00:29:24 +0300

    mock tests

---
+++ webapp/api/tests.py
@@ -0,0 +1,40 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class ApiTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_comment(self):
+        pass
+
+    def test_upvote_comment(self):
+        pass
+
+    def test_downvote_comment(self):
+        pass
+
+    def test_upvote_post(self):
+        pass
+
+    def test_downvote_post(self):
+        pass
+
+    def test_get_amount_comment_votes(self):
+        pass
+
+    def test_get_amount_post_votes(self):
+        pass
+
+    def test_get_amount_comment_upvotes(self):
+        pass
+
+    def test_get_amount_comment_downvotes(self):
+        pass
+
+    def test_get_amount_post_upvotes(self):
+        pass
+
+    def test_get_amount_post_downvotes(self):
+        pass
